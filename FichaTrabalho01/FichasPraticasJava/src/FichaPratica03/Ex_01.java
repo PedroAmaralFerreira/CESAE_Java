7ddaa247027dcbad17c5fb7fceda2930bcb4db64
Pedro Ferreira <152202794+PedroAmaralFerreira@users.noreply.github.com>
2024-06-12 16:47:04 +0100

    FichaTrabalhoCESAE
    Ficha01, 02, Extra e 03

---
+++ FichaTrabalho01/FichasPraticasJava/src/FichaPratica03/Ex_01.java
@@ -0,0 +1,14 @@
+package FichaPratica03;
+import java.util.Scanner;
+public class Ex_01 {
+    public static void main(String[] args) {
+
+        // Declarar variáveis
+        int num = 1;
+
+        while (num >=1 && num <=250){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
