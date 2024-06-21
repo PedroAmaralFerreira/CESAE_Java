d2003c4b0929f3b994834e191a6a4244fe178c00
PedroAmaralFerreira <152202794+PedroAmaralFerreira@users.noreply.github.com>
2024-06-21 16:50:24 +0100

    Updated Ex01
    - Added comments

--- FichasPraticasJava - PCE/FichaPratica05/Ex_01.java
+++ FichasPraticasJava - PCE/FichaPratica05/Ex_01.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 public class Ex_01 {
     public static void main(String[] args) {
 
+        // Importar Scanner
         Scanner input = new Scanner(System.in);
 
+        // Declarar variavel
         int[] num = new int[10];
 
+        // Ler numeros
         for (int i = 0; i < 10; i++) {
             System.out.print("Insira o número no Array["+i+"]: ");
             num[i] = input.nextInt();
         }
 
+        // Apresentar numeros
         for (int i = 0; i < 10; i++) {
             System.out.println(num[i]);
         }
